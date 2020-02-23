5f0b3ae12c7bbe246f87269a5f00141f416d3e8d
Iurie Tarlev <iura.tarlev@gmail.com>
2020-02-23 15:07:28 +0000

    added a basic test for the main App component

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../src/App';
+import Footer from '../src/components/layout/Footer';
 
 configure({ adapter: new Adapter() });
 
 it('Main application compnent renders without crashing', () => {
-  shallow(<App />);
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(Footer)).toHaveLength(1);
 });
