914b186cacaa132eb5b64513c34b8baa7494945c
Iurie Tarlev <iura.tarlev@gmail.com>
2020-03-24 19:55:47 +0000

    added extra comments for Startup api

--- tracksy/startups/api.py
+++ tracksy/startups/api.py
@@ -6,13 +6,16 @@ from .serializers import StartupSerializer
 # Lead Viewset (crud API, without specifying requests, managed by django)
 class StartupViewSet(viewsets.ModelViewSet):
     permission_classes = [
-        permissions.IsAuthenticated  # for now anybody can access any startups
+        permissions.IsAuthenticated
     ]
 
     serializer_class = StartupSerializer
 
+    # Only return the startups that the user owns
     def get_queryset(self):
         return self.request.user.startups.all()
 
+    # Override perform_create method
+    # to save the startup owner when creating a startup.
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
