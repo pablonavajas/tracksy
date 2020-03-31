d91512507c3e368a763f07bfcd5d5ce37c4800c2
davidhilman <dh1416@ic.ac.uk>
2020-03-31 19:19:56 +0100

    Updated URLs for APIs

--- tracksy/startups/urls.py
+++ tracksy/startups/urls.py
@@ -6,6 +6,6 @@ router = routers.DefaultRouter()
 router.register('api/startups', StartupViewSet, 'startups')
 urlpatterns = router.urls  # gives the list of urls registered at the route above
 
-urlpatterns.append(path('api/investments', InvestmentAPI.as_view()))
-urlpatterns.append(path('api/kpiNames', KpiNameAPI.as_view()))
-urlpatterns.append(path('api/financials', FinancialAPI.as_view()))
+urlpatterns.append(path('api/investments/', InvestmentAPI.as_view()))
+urlpatterns.append(path('api/kpiNames/', KpiNameAPI.as_view()))
+urlpatterns.append(path('api/financials/', FinancialAPI.as_view()))
