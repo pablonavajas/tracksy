545a02aeba3bd28c51f156729af47b291e7c140f
Iurie Tarlev <iura.tarlev@gmail.com>
2020-04-03 00:57:09 +0100

    refactored Alerts to a functional component

--- tracksy/frontend/src/components/layout/Alerts.js
+++ tracksy/frontend/src/components/layout/Alerts.js
@@ -1,49 +1,40 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { withAlert } from "react-alert";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-export class Alerts extends Component {
-  static propTypes = {
-    error: PropTypes.object.isRequired,
-    message: PropTypes.object.isRequired
-  };
-
-  // prevProps (how does this get pulled in)
-  componentDidUpdate(prevProps) {
-    //if props have changed
-    const { error, alert, message } = this.props;
-    if (error !== prevProps.error) {
-      if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
-      else if (error.msg.ownership)
-        alert.error(`Ownership: ${error.msg.ownership.join()}`);
-      else if (error.msg.website)
-        alert.error(`Website: ${error.msg.website.join()}`);
-      else if (error.msg.startupEmail)
-        alert.error(`Startup Email: ${error.msg.startupEmail.join()}`);
-      else if (error.msg.revenue)
-        alert.error(`Revenue: ${error.msg.revenue.join()}`);
-      else if (error.msg.cashBalance)
-        alert.error(`Cash Balance: ${error.msg.cashBalance.join()}`);
-      else if (error.msg.monthlyBurn)
-        alert.error(`Cash Balance: ${error.msg.monthlyBurn.join()}`);
-      else if (error.msg.non_field_errors) {
-        alert.error(error.msg.non_field_errors.join());
-      } else if (error.msg.username) {
-        alert.error(error.msg.username.join());
-      }
+function Alerts({ error, message, alert }) {
+  useEffect(() => {
+    if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+    else if (error.msg.ownership)
+      alert.error(`Ownership: ${error.msg.ownership.join()}`);
+    else if (error.msg.website)
+      alert.error(`Website: ${error.msg.website.join()}`);
+    else if (error.msg.startupEmail)
+      alert.error(`Startup Email: ${error.msg.startupEmail.join()}`);
+    else if (error.msg.revenue)
+      alert.error(`Revenue: ${error.msg.revenue.join()}`);
+    else if (error.msg.cashBalance)
+      alert.error(`Cash Balance: ${error.msg.cashBalance.join()}`);
+    else if (error.msg.monthlyBurn)
+      alert.error(`Cash Balance: ${error.msg.monthlyBurn.join()}`);
+    else if (error.msg.non_field_errors) {
+      alert.error(error.msg.non_field_errors.join());
+    } else if (error.msg.username) {
+      alert.error(error.msg.username.join());
     }
 
-    if (message !== prevProps.message) {
-      if (message.succ) alert.success(message.succ);
-      if (message.fail) alert.error(message.fail);
-    }
-  }
+    if (message.succ) alert.success(message.succ);
+    if (message.fail) alert.error(message.fail);
+  });
 
-  render() {
-    return <Fragment />;
-  }
+  return <Fragment />;
 }
+
+Alerts.propTypes = {
+  error: PropTypes.object.isRequired,
+  message: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
   error: state.errors,
