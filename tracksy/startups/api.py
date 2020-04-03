ee15620fe9dcc5c7f837410b4c50f9941d1cd03b
davidhilman <dh1416@ic.ac.uk>
2020-04-03 14:03:19 +0100

    List api error handling corrected

--- tracksy/startups/api.py
+++ tracksy/startups/api.py
@@ -45,42 +45,40 @@ def get_startup(user, pk=None):
 
 def process_field(data, startup, Serializer, field):
     data['startupId'] = startup.pk
-    try:
-        field_id = data.get('id')
-        if field_id is None:
-            serializer = Serializer(data=data)
-        else:
-            instance = getattr(startup, field).get(pk=field_id)
-            serializer = Serializer(instance=instance, data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return serializer.data
-    except (ValidationError, ObjectDoesNotExist) as err:
-        print("Error ({}) when adding object: {}".format(err, data))
-
-def startup_field_proccesor(request, startupId, Serializer, field, many=True):
-    data = request.data
-    startup = get_startup(request.user, pk=startupId)
-    if many is True:
-        if type(data) is dict:
-            data = [data]
-        for data_item in data:
-            process_field(data_item, startup, Serializer, field)
-        query = getattr(startup, field).all()
-        serializer = Serializer(data=query, many=many)
-        serializer.is_valid()
-        return serializer.data
+    field_id = data.get('id')
+    if field_id is None:
+        serializer = Serializer(data=data)
     else:
-        return process_field(data, startup, Serializer, field)
+        instance = getattr(startup, field).get(pk=field_id)
+        serializer = Serializer(instance=instance, data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return serializer.data
 
+
+def list_processor(request, startupId, Serializer, field):
+    data = request.data
+    if type(data) is not list:
+        raise ValidationError("Expecting a list for this API")
+
+    response = {"added": [], "errors": []}
+    startup = get_startup(request.user, pk=startupId)
+    for data_item in data:
+        try:
+            response["added"].append(process_field(data_item, startup, Serializer, field))
+        except ValidationError as err:
+            response["errors"].append(err.detail)
+        except ObjectDoesNotExist as err:
+            response["errors"].append({"message": str(err)})
+
+    return response
 
 
 class InvestmentAPI(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, startupId):
-        response = startup_field_proccesor(request, startupId, InvestmentSerializer,
-                                           "investments", many=True)
+        response = list_processor(request, startupId, InvestmentSerializer, "investments")
         return Response(response)
 
     def delete(self, request, startupId, pk):
@@ -94,8 +92,7 @@ class KpiNameAPI(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, startupId):
-        response = startup_field_proccesor(request, startupId, KpiNameSerializer,
-                                           "kpinames", many=True)
+        response = list_processor(request, startupId, KpiNameSerializer, "kpinames")
         return Response(response)
 
     def delete(self, request, startupId, pk):
@@ -109,8 +106,8 @@ class FinancialAPI(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, startupId):
-        response = startup_field_proccesor(request, startupId, FinancialSerializer,
-                                           "financials", many=False)
+        startup = get_startup(request.user, pk=startupId)
+        response = process_field(request.data, startup, FinancialSerializer, "financials")
         return Response(response)
 
     def delete(self, request, startupId, pk):
