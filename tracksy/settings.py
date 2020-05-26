829c77c6faedfb5d3750830a8751e5afa0fa3580
Pablo Navajas Helguero <P.Navajas-Helguero@warwick.ac.uk>
2020-05-26 20:07:22 +0200

    template dirs files updated

--- tracksy/settings.py
+++ tracksy/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'tracksy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, './frontend/templates/frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
